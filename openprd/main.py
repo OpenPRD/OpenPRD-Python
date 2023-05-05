798e73fb21ebb0ca848066a514fa330bda8796b2
Walter Branning <walter@oddlygeek.com>
2023-05-04 21:12:25 -0500

    small restructering

---
+++ openprd/main.py
@@ -0,0 +1,28 @@
+from openprd import OpenPRDFile
+import argparse
+
+arg_parser = argparse.ArgumentParser(
+    prog="OpenPRD-Python",
+    description="Reads OpenPRD formatted files and returns JSON object.",
+)
+
+arg_parser.add_argument('-b', '--build', required=False, help='Build prd file',
+                        action='store_true')
+
+arg_parser.add_argument('-o', '--outputfile', metavar="File to Output",
+                        help='Output JSON to file', required=False, 
+                        )
+
+arg_parser.add_argument('-v', '--version', action='version')
+arg_parser.add_argument('input_file')
+# arg_parser.add_argument('output_file')
+
+
+
+def main(args):
+    prd = OpenPRDFile(args.input_file)
+    if args.outputfile:
+        prd.write_json(args.outputfile)
+
+if __name__ == "__main__":
+    main(arg_parser.parse_args())
